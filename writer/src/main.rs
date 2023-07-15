ea94dd7c736972401bd1ff029f2b8e427b96cdaf
Matthew Sherborne <msherborne@gmail.com>
2023-07-16 01:56:21 +1000

    Can now write some code. yay

--- writer/src/main.rs
+++ writer/src/main.rs
@@ -1,3 +1,15 @@
+use quote::quote;
+use rust_format::{Formatter, PrettyPlease};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let source = quote! {
+        fn main() {
+            println!("Hello World!");
+        }
+    };
+
+    let formatted_code = PrettyPlease::default().format_tokens(source).unwrap();
+
+    fs::write("output.rs", formatted_code).expect("Unable to write file");
 }
